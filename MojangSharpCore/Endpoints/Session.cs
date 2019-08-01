8ab414891215e9b90caa51f37118484c720cfbc7
ChangFeng <chang_196700@hotmail.com>
2019-08-02 02:32:13 +0800

    Add Session
    Signed-off-by: ChangFeng <chang_196700@hotmail.com>

---
+++ MojangSharpCore/Endpoints/Session.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using MojangSharpCore.Api;
+using MojangSharpCore.Responses;
+using Newtonsoft.Json.Linq;
+
+namespace MojangSharpCore.Endpoints
+{
+    public class Join : IEndpoint<Response>
+    {
+        public Join(string accessToken, string selectedProfile, string serverId)
+        {
+            Address = new Uri("https://sessionserver.mojang.com/session/minecraft/join");
+            Arguments.Add(accessToken);
+            Arguments.Add(selectedProfile);
+            Arguments.Add(serverId);
+        }
+
+        public override async Task<Response> PerformRequestAsync()
+        {
+            PostContent = new JObject(
+                new JProperty("accessToken", Arguments[0]),
+                new JProperty("selectedProfile", Arguments[1]),
+                new JProperty("serverId", Arguments[2])).ToString();
+
+            Response = await Requester.Post(this);
+
+            if (string.IsNullOrWhiteSpace(Response.RawMessage))
+            {
+                return new Response(Response) { IsSuccess = true };
+            }
+            else
+            {
+                return new Response(Error.GetError(Response));
+            }
+        }
+    }
+
+    public class HasJoin : IEndpoint<HasJoinResponse>
+    {
+        public HasJoin(string username, string serverId, string ip)
+        {
+            Address = new Uri($"https://sessionserver.mojang.com/session/minecraft/hasJoined?username={username}&serverId={serverId}&ip={ip}");
+        }
+
+        public override async Task<HasJoinResponse> PerformRequestAsync()
+        {
+            Response = await Requester.Get(this);
+
+            if (Response.IsSuccess)
+            {
+                JObject res = JObject.Parse(Response.RawMessage);
+                var obj = res.ToObject<HasJoinResponse>();
+
+                return new HasJoinResponse(Response)
+                {
+                    Id = obj.Id,
+                    Name = obj.Name,
+                    Properties = obj.Properties
+                };
+            }
+            else
+            {
+                return new HasJoinResponse(Error.GetError(Response));
+            }
+        }
+    }
+}
---
+++ MojangSharpCore/Responses/SessionResponse.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace MojangSharpCore.Responses
+{
+    public class HasJoinResponse : Response
+    {
+        internal HasJoinResponse(Response response) : base(response)
+        {
+        }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("properties")]
+        public List<Property> Properties { get; set; }
+
+        public class Property
+        {
+            /// <summary>
+            /// Property name
+            /// </summary>
+            [JsonProperty("name")]
+            public string Name { get; internal set; }
+
+            /// <summary>
+            /// Property value
+            /// </summary>
+            [JsonProperty("value")]
+            public string Value { get; internal set; }
+        }
+    }
+}
